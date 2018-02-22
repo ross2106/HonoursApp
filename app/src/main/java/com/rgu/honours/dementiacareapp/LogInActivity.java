ca61cbc94e28dc9714dfccb864aa9bc414befc88
ROSS HENDERSON <ross1994@hotmail.co.uk>
2018-02-22 15:57:03 +0000

    Further dev on log in/sign up pages

---
+++ app/src/main/java/com/rgu/honours/dementiacareapp/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.rgu.honours.dementiacareapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ross1 on 22/02/2018.
+ */
+
+public class LogInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_layout);
+    }
+}
--- app/src/main/java/com/rgu/honours/dementiacareapp/MainActivity.java
+++ app/src/main/java/com/rgu/honours/dementiacareapp/MainActivity.java
@@ -1,13 +1,40 @@
 package com.rgu.honours.dementiacareapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button welcomeLogIn, welcomeSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //defining buttons
+        welcomeLogIn = (Button) findViewById(R.id.logIn);
+        welcomeSignUp = (Button) findViewById(R.id.signUp);
+
+        //Add click listener to button
+        welcomeLogIn.setOnClickListener(this);
+        welcomeSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch(view.getId()){
+            case R.id.logIn : i = new Intent(this, LogInActivity.class); startActivity(i); break;
+            default: break;
+
+        }
+
+
+
     }
 }
